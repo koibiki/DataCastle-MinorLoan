385fe66c9c6effc314f2b8fb7734f30e78e470e3
chengli <lcheng.ext@orange.com>
2017-11-27 20:03:13 +0800

    category discretization.

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from feature_engineering.data_loader import DataLoader
 from feature_engineering.null_handler import NullHandler
 from feature_engineering.visualize_null import Visualize
-from feature_engineering.discretization import Discretizator
+from feature_engineering.numeric_discretization import Discretizator
 
 # DataLoader.rank()
 
